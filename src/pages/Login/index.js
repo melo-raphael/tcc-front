23048226e2af45faa5495c996d8ab2c701db1136
Lincoln Teixeira <lincoln@vexter.com.br>
2020-05-08 13:08:56 -0300

    creating register page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 import {FaSignInAlt} from 'react-icons/fa';
-import LoginImage from '../../assets/login-image.svg';
-import Logo from '../../assets/logo.svg';
+import loginImage from '../../assets/login-image.svg';
+import logo from '../../assets/logo.svg';
 
 export default function Login () {
 
     return (
         <div className="login-container">
-            <img src={LoginImage} alt=""/>
+            <img src={loginImage} alt=""/>
 
             <section className="form">
-                <img src={Logo} alt=""/>
+                <img src={logo} alt=""/>
 
                 <form>
                     <h1>Iniciar Sessão</h1>
@@ -22,10 +23,10 @@ export default function Login () {
                     <input placeholder="Senha" type="password"/>
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="#">
+                    <Link className="default-link" to="/register">
                         <FaSignInAlt size= {16} color="#8A05BE" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>                
             </section>
         </div>
