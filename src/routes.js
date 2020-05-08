1836aa268d5fdccf307106ac17ac589d971743cd
Lincoln Teixeira <lincoln@vexter.com.br>
2020-05-08 13:08:36 -0300

    adding new route and new global style

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 const Routes = () => (
     <BrowserRouter> 
         <Switch>
             <Route exact path ="/" component={Login}/>
-            <Route />
+            <Route path="/register" component={Register}/>
         </Switch>
     </BrowserRouter>
 );
