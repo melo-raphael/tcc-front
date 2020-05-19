bc0d5d9e9de382cd9a604b7f67d254f399de3a9c
Lincoln Teixeira <lincoln@vexter.com.br>
2020-05-19 20:29:33 -0300

    consuming data from backend and removing mocked informations

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FaPowerOff } from 'react-icons/fa';
+import api from '../../services/api';
 
 export default function Dashboard () {
+    
+    const [assets, setAssets] = useState([]);
+    
+    useEffect(() => {
+        api.get('/order/managament/assets').then(receivedAssets => {
+            setAssets(receivedAssets.data.data);
+        });
+    }, []);
+    // add user id no array de dependencias ou algo do tipo
+
     return (
         <div className="dashboard-container">
             <header>
@@ -23,17 +34,18 @@ export default function Dashboard () {
             <h1>Ativos disponíveis</h1>
 
             <ul>
-                <li>
+                { assets.map(asset => (
+                <li key={ asset.name }>
                     <div className="card-header">
                         <div className="asset-image">
-                            <img src="https://app.vexter.com.br/assets/img/assets/NO-IMAGE-ASSETS.PNG" alt="imagem do ativo"/>
+                            <img src={ asset.imageUrl } alt="imagem do ativo"/>
                         </div>
 
                         <div className="asset-info">
-                            <h3>Magazine Luiza</h3>
+                            <h3>{ asset.name }</h3>
 
                             <div className="info-details">
-                                <span>MGLU3</span>
+                                <span>{ asset.symbol }</span>
                                 <span>09/05/2020</span>
                             </div>
                         </div>
@@ -71,243 +83,9 @@ export default function Dashboard () {
                         </div>
                     </div>
                 </li>
+                ))}
                 {/* so pra teste */}
-                <li>
-                    <div className="card-header">
-                        <div className="asset-image">
-                            <img src="https://app.vexter.com.br/assets/img/assets/NO-IMAGE-ASSETS.PNG" alt="imagem do ativo"/>
-                        </div>
-
-                        <div className="asset-info">
-                            <h3>Magazine Luiza</h3>
-
-                            <div className="info-details">
-                                <span>MGLU3</span>
-                                <span>09/05/2020</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span className="divider"></span>
-                    
-                    <div className="graph-container">
-                        <div className="graph-header">
-                            <span>Variação (1s)</span>
-                            <span>5.04%</span>
-                        </div>
-                        <canvas>
-                            /* aqui deve vir o grafico */
-                        </canvas>
-                    </div>
-
-                    <div className="buy-sell-area">
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Vender
-                            </div>
-                            <div className="price">
-                                <span>55,23</span>
-                            </div>
-                        </div>
-
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Comprar
-                            </div>
-                            <div className="price">
-                                <span>56,00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div className="card-header">
-                        <div className="asset-image">
-                            <img src="https://app.vexter.com.br/assets/img/assets/NO-IMAGE-ASSETS.PNG" alt="imagem do ativo"/>
-                        </div>
-
-                        <div className="asset-info">
-                            <h3>Magazine Luiza</h3>
-
-                            <div className="info-details">
-                                <span>MGLU3</span>
-                                <span>09/05/2020</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span className="divider"></span>
-                    
-                    <div className="graph-container">
-                        <div className="graph-header">
-                            <span>Variação (1s)</span>
-                            <span>5.04%</span>
-                        </div>
-                        <canvas>
-                            /* aqui deve vir o grafico */
-                        </canvas>
-                    </div>
-
-                    <div className="buy-sell-area">
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Vender
-                            </div>
-                            <div className="price">
-                                <span>55,23</span>
-                            </div>
-                        </div>
-
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Comprar
-                            </div>
-                            <div className="price">
-                                <span>56,00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div className="card-header">
-                        <div className="asset-image">
-                            <img src="https://app.vexter.com.br/assets/img/assets/NO-IMAGE-ASSETS.PNG" alt="imagem do ativo"/>
-                        </div>
-
-                        <div className="asset-info">
-                            <h3>Magazine Luiza</h3>
-
-                            <div className="info-details">
-                                <span>MGLU3</span>
-                                <span>09/05/2020</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span className="divider"></span>
-                    
-                    <div className="graph-container">
-                        <div className="graph-header">
-                            <span>Variação (1s)</span>
-                            <span>5.04%</span>
-                        </div>
-                        <canvas>
-                            /* aqui deve vir o grafico */
-                        </canvas>
-                    </div>
-
-                    <div className="buy-sell-area">
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Vender
-                            </div>
-                            <div className="price">
-                                <span>55,23</span>
-                            </div>
-                        </div>
-
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Comprar
-                            </div>
-                            <div className="price">
-                                <span>56,00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div className="card-header">
-                        <div className="asset-image">
-                            <img src="https://app.vexter.com.br/assets/img/assets/NO-IMAGE-ASSETS.PNG" alt="imagem do ativo"/>
-                        </div>
-
-                        <div className="asset-info">
-                            <h3>Magazine Luiza</h3>
-
-                            <div className="info-details">
-                                <span>MGLU3</span>
-                                <span>09/05/2020</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span className="divider"></span>
-                    
-                    <div className="graph-container">
-                        <div className="graph-header">
-                            <span>Variação (1s)</span>
-                            <span>5.04%</span>
-                        </div>
-                        <canvas>
-                            /* aqui deve vir o grafico */
-                        </canvas>
-                    </div>
-
-                    <div className="buy-sell-area">
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Vender
-                            </div>
-                            <div className="price">
-                                <span>55,23</span>
-                            </div>
-                        </div>
-
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Comprar
-                            </div>
-                            <div className="price">
-                                <span>56,00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div className="card-header">
-                        <div className="asset-image">
-                            <img src="https://app.vexter.com.br/assets/img/assets/NO-IMAGE-ASSETS.PNG" alt="imagem do ativo"/>
-                        </div>
-
-                        <div className="asset-info">
-                            <h3>Magazine Luiza</h3>
-
-                            <div className="info-details">
-                                <span>MGLU3</span>
-                                <span>09/05/2020</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span className="divider"></span>
-                    
-                    <div className="graph-container">
-                        <div className="graph-header">
-                            <span>Variação (1s)</span>
-                            <span>5.04%</span>
-                        </div>
-                        <canvas>
-                            /* aqui deve vir o grafico */
-                        </canvas>
-                    </div>
-
-                    <div className="buy-sell-area">
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Vender
-                            </div>
-                            <div className="price">
-                                <span>55,23</span>
-                            </div>
-                        </div>
-
-                        <div className="button-buy-sell">
-                            <div className="buy-sell-header">
-                                Comprar
-                            </div>
-                            <div className="price">
-                                <span>56,00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+               
 
             </ul>
         </div>
